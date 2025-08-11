d931b3fc76e3e7291e48bc7bd8d0a45e25fbb9a4
axelluribe <axelluribe13@gmail.com>
2025-08-11 18:28:25 +0000

    [restructuración de codigo] modificación de la linea 12 a la linea 21

--- src/app.js
+++ src/app.js
@@ -1,24 +1,24 @@
 
-  import "bootstrap";
+import "bootstrap";
 import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  let sujeto = ['mi perro', 'mi abuela', 'el cartero', 'Mi hijo'];
-  let verb = ['se comio ', 'perdio', 'estrello', 'rompio'];
-  let predicado = ['mi tarea', 'las llaves', 'el carro', 'el telefono'];
+window.onload = function () {
 
-  
-  let generarNumeroRandom = (array) => {
+let sujeto = ["mi perro", "mi abuela", "el cartero", "mi hijo"];
+  let verb = ["se comio ", "perdio", "estrello", "rompio"];
+  let predicado = ["mi tarea", "las llaves", "el carro", "el telefono"];
+
+  let generarNumeroRandom = function (array) {
     return Math.floor(Math.random() * array.length);
-  }
+  };
 
   let numeroRandom = generarNumeroRandom(sujeto);
   console.log("Número random:", numeroRandom);
-  let excusa = sujeto[numeroRandom] + " " + verb[numeroRandom] + " " + predicado[numeroRandom];
-  console.log("frase:", excusa);
 
+  let excusa = `${sujeto[numeroRandom]} ${verb[numeroRandom]} ${predicado[numeroRandom]}`
 
+  console.log("Frase:", excusa);
   document.getElementById("excusa").innerHTML = excusa;
-}
+};
